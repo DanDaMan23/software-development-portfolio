cc7f3a57373400704760e1fd947e6019dbaa9217
DanDaMan23 <danlawrcons.gmail.com>
2024-01-15 12:27:28 -0600

    Added proper navigation items

--- src/sections/header/navigation-area/navigation-area.tsx
+++ src/sections/header/navigation-area/navigation-area.tsx
@@ -11,18 +11,54 @@ export default function NavigationArea({
 }: NavigationAreaProps) {
   const [showNavigation, setShowNavigation] = useState(false)
 
+  const navigationItems = [
+    {
+      label: "About",
+      onClickEvent: () => {
+        toggleNavigationHandler()
+      }
+    },
+    {
+      label: "Experience",
+      onClickEvent: () => {
+        toggleNavigationHandler()
+      }
+    },
+    {
+      label: "Projects",
+      onClickEvent: () => {
+        toggleNavigationHandler()
+      }
+    },
+    {
+      label: "Education",
+      onClickEvent: () => {
+        toggleNavigationHandler()
+      }
+    },
+    {
+      label: "Contact",
+      onClickEvent: () => {
+        toggleNavigationHandler()
+      }
+    }
+  ]
+
   useEffect(() => {
     setShowNavigation(isOpen)
   }, [isOpen])
 
   return (
     <div
-      className={`flex flex-col gap-3 py-3 border-y border-light-gray ${
+      className={`flex flex-col border-y border-light-gray ${
         showNavigation ? "block" : "hidden"
       }`}
     >
-      <button onClick={toggleNavigationHandler}>About</button>
-      <button onClick={toggleNavigationHandler}>Experience</button>
+      {navigationItems.map(({ label, onClickEvent }, index) => (
+        <button key={index} onClick={onClickEvent} className='border-b py-3'>
+          {label}
+        </button>
+      ))}
     </div>
   )
 }

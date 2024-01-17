023f173970d3d8b433ccbd6ee867a41686dbe6dc
DanDaMan23 <danlawrcons.gmail.com>
2024-01-17 17:43:51 -0600

    Extracted magic text into json file for work experience

--- src/sections/experience/experience.tsx
+++ src/sections/experience/experience.tsx
@@ -1,9 +1,10 @@
 import Work from "./work"
+import text from "./text.json"
 
 export default function Experience() {
   return (
     <div>
-      <p className='text-center font-bold text-2xl my-3'>Experience</p>
+      <p className='text-center font-bold text-2xl my-3'>{text.mainTitle}</p>
       <Work />
     </div>
   )
--- src/sections/experience/work.tsx
+++ src/sections/experience/work.tsx
@@ -1,27 +1,14 @@
 import ExperienceCard from "./components/experience-card"
 
+import text from "./text.json"
+
 export default function Work() {
   return (
     <div>
-      <p className='text-xl font-bold mb-3'>Work</p>
-      <ExperienceCard
-        date='October 2021 - June 2023'
-        workplace='Cambrian Credit Union'
-        position='Programmer 1'
-        description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
-      quod ipsam molestias itaque voluptatibus quo. Rerum assumenda
-      explicabo consequatur nobis, facere sint dolore laborum consectetur
-      obcaecati repellat, quae unde distinctio.'
-        technologyUsed={[
-          "JavaScript",
-          "React",
-          "Redux",
-          "C#",
-          ".NET Core",
-          "WBDK",
-          "SQL"
-        ]}
-      />
+      <p className='text-xl font-bold mb-3'>{text.work.title}</p>
+      {text.work.workList.map((work) => (
+        <ExperienceCard {...work} />
+      ))}
     </div>
   )
 }

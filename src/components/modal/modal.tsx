f4f85fa192bc6a7b7db653b9c99713f57094c569
DanDaMan23 <danlawrcons.gmail.com>
2024-01-17 15:46:37 -0600

    Implemented Modal in the experience card

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -19,13 +19,13 @@ export default function Modal({
 
   return (
     <div className='fixed inset-0 overflow-y-auto bg-light-gray bg-opacity-60 flex justify-center items-center'>
-      <div className='bg-black w-[75vw] rounded-lg'>
+      <div className='bg-black w-[75vw] rounded-lg modal-content'>
         <div className='flex justify-end p-4 border-b'>
           <button onClick={onClose}>
             <FontAwesomeIcon icon={faX} />
           </button>
         </div>
-        <div className='h-[75vh] overflow-y-auto'>{children}</div>
+        <div className='max-h-[75vh] overflow-y-auto'>{children}</div>
         {footer &&
           cloneElement(footer, {
             className: `p-3 border-t border-light-gray ${
--- src/sections/experience/components/experience-card.tsx
+++ src/sections/experience/components/experience-card.tsx
@@ -1,3 +1,8 @@
+import { useState } from "react"
+import Modal from "../../../components/modal/modal"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons"
+
 interface ExperienceCardProps {
   date: string
   workplace: string
@@ -13,10 +18,18 @@ export default function ExperienceCard({
   description,
   technologyUsed
 }: ExperienceCardProps) {
+  const [isOpenModal, setIsOpenModal] = useState(false)
+
+  const openModalHandler = () => setIsOpenModal(true)
+
+  const closeModalHandler = () => setIsOpenModal(false)
+
   return (
     <div>
       <p className='text-sm'>{date}</p>
-      <p className='text-xl font-bold'>{workplace}</p>
+      <button className='text-xl font-bold' onClick={openModalHandler}>
+        {workplace} <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+      </button>
       <p className='font-bold'>{position}</p>
       <p className='truncate'>{description}</p>
       <div className='flex flex-nowrap justify-start gap-3 py-2 pl-1 truncate'>
@@ -29,6 +42,27 @@ export default function ExperienceCard({
           </p>
         ))}
       </div>
+      <Modal
+        isOpen={isOpenModal}
+        onClose={closeModalHandler}
+        footer={<div>Helo from the other side</div>}
+      >
+        <div className='p-3'>
+          <p className='text-xl font-bold'>{workplace}</p>
+          <p className='font-bold'>{position}</p>
+          <p>{description}</p>
+          <div className='flex justify-start gap-3 py-2 pl-1'>
+            {technologyUsed.map((value, index) => (
+              <p
+                key={index}
+                className='bg-dark-gray p-1 rounded-full ring-1 ring-light-gray text-xs'
+              >
+                {value}
+              </p>
+            ))}
+          </div>
+        </div>
+      </Modal>
     </div>
   )
 }

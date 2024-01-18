97fc49eb6b95e0001607367ceb3f7ce766cf0798
DanDaMan23 <danlawrcons.gmail.com>
2024-01-18 17:17:14 -0600

    Project Card refactored

--- src/sections/projects/project-card.tsx
+++ src/sections/projects/project-card.tsx
@@ -6,6 +6,8 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Modal from "../../components/modal/modal"
 
+import text from "./text.json"
+
 interface ProjectCardProps {
   name: string
   imagePath: string
@@ -27,51 +29,57 @@ export default function ProjectCard({
 
   const closeModalHandler = () => setIsModalOpen(false)
 
+  const ProjectImage = () => (
+    <img src={imagePath} alt={name} className='rounded-md my-3' />
+  )
+
+  const TechStackPills = () => (
+    <div className='flex flex-nowrap justify-start gap-3 py-2 pl-1 truncate'>
+      {techStack.map((value, index) => (
+        <p
+          key={index}
+          className='bg-dark-gray p-1 rounded-full ring-1 ring-light-gray text-xs'
+        >
+          {value}
+        </p>
+      ))}
+    </div>
+  )
+
+  const modalFooter =
+    sourceCodeLink || liveLink ? (
+      <div className='flex justify-around'>
+        <a href={sourceCodeLink} target='_blank' rel='noopener noreferrer'>
+          {text.sourceCodeLabel} <FontAwesomeIcon icon={faLink} />
+        </a>
+        <a href={liveLink} target='_blank' rel='noopener noreferrer'>
+          {text.liveLinkLabel} <FontAwesomeIcon icon={faLink} />
+        </a>
+      </div>
+    ) : undefined
+
+  const ProjectCardModal = () => (
+    <Modal
+      isOpen={isModalOpen}
+      onClose={closeModalHandler}
+      footer={modalFooter}
+    >
+      <div className='p-3'>
+        <p className='text-xl font-bold'>{name}</p>
+        <ProjectImage />
+        <TechStackPills />
+      </div>
+    </Modal>
+  )
+
   return (
     <div>
       <button className='text-xl font-bold my-3' onClick={openModalHandler}>
         {name} <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
       </button>
-      <img src={imagePath} alt={name} className='rounded-md my-3' />
-      <div className='flex flex-nowrap justify-start gap-3 py-2 pl-1 truncate'>
-        {techStack.map((value, index) => (
-          <p
-            key={index}
-            className='bg-dark-gray p-1 rounded-full ring-1 ring-light-gray text-xs'
-          >
-            {value}
-          </p>
-        ))}
-      </div>
-      <Modal
-        isOpen={isModalOpen}
-        onClose={closeModalHandler}
-        footer={
-          <div className='flex justify-around'>
-            <a href={sourceCodeLink}>
-              Source Code <FontAwesomeIcon icon={faLink} />
-            </a>
-            <a href={liveLink}>
-              Live Link <FontAwesomeIcon icon={faLink} />
-            </a>
-          </div>
-        }
-      >
-        <div className='p-3'>
-          <p className='text-xl font-bold'>{name}</p>
-          <img src={imagePath} alt={name} className='rounded-md my-3' />
-          <div className='flex flex-nowrap justify-start gap-3 py-2 pl-1 truncate'>
-            {techStack.map((value, index) => (
-              <p
-                key={index}
-                className='bg-dark-gray p-1 rounded-full ring-1 ring-light-gray text-xs'
-              >
-                {value}
-              </p>
-            ))}
-          </div>
-        </div>
-      </Modal>
+      <ProjectImage />
+      <TechStackPills />
+      <ProjectCardModal />
     </div>
   )
 }
